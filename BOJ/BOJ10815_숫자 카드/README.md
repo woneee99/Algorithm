b5a5c3076c76a01cff7a3153ceabef5165489d72
wonee <hwang9821@icloud.com>
2022-06-22 14:47:54 +0900

    solution

---
+++ BOJ/BOJ10815_숫자 카드/README.md
@@ -0,0 +1,10 @@
+# 10815 숫자 카드
+
+### solution
+* 이분탐색으로 풀었는데 단순 배열로 구현 가능(더 빠르다)
+
+### Classification
+* 이분탐색
+
+### link
+https://www.acmicpc.net/problem/10815
---
+++ BOJ/BOJ10815_숫자 카드/boj10815.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj10815 {
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) {
+			int x = Integer.parseInt(st.nextToken());
+			
+			sb.append(binary(x, 0, n-1)).append(" ");
+		}
+		System.out.println(sb.toString());
+	}
+	static int binary(int key, int start, int end) {
+		int mid;
+		
+		while(start<=end) {
+			mid = (start+end)/2;
+			if(key == arr[mid]) {
+				return 1;
+			}
+			else if(key < arr[mid]) {
+				end = mid-1;
+			}
+			else {
+				start = mid + 1;
+			}
+		}
+		
+		return 0;
+	}
+}
