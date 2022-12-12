48090bbde494f684e9579707083233fb533992e3
DESKTOP-AVA9CAM\wonee <woneee99@gmail.com>
2022-12-12 15:27:40 +0900

    solution

---
+++ BOJ/BOJ11758_CCW/README.md
@@ -0,0 +1,11 @@
+# 11758 CCW
+
+### solution
+* 신발끈 공식을 안다면 쉽게 풀리는 문제
+https://velog.io/@woneee99/%EA%B8%B0%ED%95%98%ED%95%99-CCW
+
+### Classification
+* 기하학
+
+### link
+https://www.acmicpc.net/problem/11758
---
+++ BOJ/BOJ11758_CCW/boj11758.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj11758 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x2 = Integer.parseInt(st.nextToken());
+		int y2 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x3 = Integer.parseInt(st.nextToken());
+		int y3 = Integer.parseInt(st.nextToken());
+		
+		int result = ((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((x2* y1) + (x3 * y2) + (x1 * y3));
+		if(result > 0) {
+			System.out.println(1);
+		}
+		else if(result < 0) {
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(0);
+		}
+	}
+
+}
